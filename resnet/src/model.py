9b4d6efc0a29e91a56cf6f3bee618b31d0a39b08
Lea Nguyen <lea.nguyen.1@etu.u-bordeaux.fr>
2026-04-20 11:30:42 +0200

    nouveau fichier : src/chat.py 	modifié :         src/model.py

--- resnet/src/model.py
+++ resnet/src/model.py
@@ -1,7 +1,12 @@
 #Packages
 import torch
 import torch.nn as nn
-
+import torch.optim as optim
+import torch.nn.functional as F
+import torchvision
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+import numpy as np
 
 # Bloc résiduel resnet18 de geeksforgeeks
 
@@ -99,93 +104,11 @@ class ResNet18(nn.Module): #de geeksforgeeks
         return out
 
 
-#### Version chat : différence dans les strides. 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model = ResNet18().to(device)
+print(model)
 
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(model.parameters(), lr=0.1, momentum=0.9, weight_decay=5e-4)
+scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=30, gamma=0.1)
 
-class BasicBlock(nn.Module):
-    expansion = 1
-
-    def __init__(self, in_channels, out_channels, stride=1):
-        super().__init__()
-
-        self.conv1 = nn.Conv2d(
-            in_channels, out_channels,
-            kernel_size=3,
-            stride=stride,
-            padding=1,
-            bias=False
-        )
-        self.bn1 = nn.BatchNorm2d(out_channels)
-        self.relu = nn.ReLU(inplace=True)
-
-        self.conv2 = nn.Conv2d(
-            out_channels, out_channels,
-            kernel_size=3,
-            stride=1,
-            padding=1,
-            bias=False
-        )
-        self.bn2 = nn.BatchNorm2d(out_channels)
-
-        # shortcut
-        self.shortcut = nn.Sequential()
-        if stride != 1 or in_channels != out_channels:
-            self.shortcut = nn.Sequential(
-                nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride, bias=False),
-                nn.BatchNorm2d(out_channels)
-            )
-
-    def forward(self, x):
-        out = self.relu(self.bn1(self.conv1(x)))
-        out = self.bn2(self.conv2(out))
-        out += self.shortcut(x)
-        out = self.relu(out)
-        return out
-
-
-class ResNet18(nn.Module): 
-    def __init__(self, block, layers, num_classes=3):
-        super().__init__()
-
-        self.in_channels = 64
-
-        # Stem
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False)
-        self.bn1 = nn.BatchNorm2d(64)
-        self.relu = nn.ReLU(inplace=True)
-        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-
-        # Residual layers
-        self.layer1 = self._make_layer(block, 64,  layers[0], stride=1)
-        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
-        self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
-        self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
-
-        # Head
-        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
-        self.fc = nn.Linear(512 * block.expansion, num_classes)
-
-    def _make_layer(self, block, out_channels, blocks, stride):
-        strides = [stride] + [1] * (blocks - 1)
-
-        layers = []
-        for s in strides:
-            layers.append(block(self.in_channels, out_channels, s))
-            self.in_channels = out_channels * block.expansion
-
-        return nn.Sequential(*layers)
-
-    def forward(self, x):
-        x = self.relu(self.bn1(self.conv1(x)))
-        x = self.maxpool(x)
-
-        x = self.layer1(x)
-        x = self.layer2(x)
-        x = self.layer3(x)
-        x = self.layer4(x)
-
-        x = self.avgpool(x)
-        x = torch.flatten(x, 1)
-        x = self.fc(x)
-
-        return x
