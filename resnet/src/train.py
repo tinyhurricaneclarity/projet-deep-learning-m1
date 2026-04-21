4a135ed6718f0cc0c287960d6ce584649eb8d13d
Lea Nguyen <lea.nguyen.1@etu.u-bordeaux.fr>
2026-04-21 15:30:05 +0200

    nouveau fichier : resnet/src/Explications.md 	modifié :         resnet/src/train.py

--- resnet/src/train.py
+++ resnet/src/train.py
@@ -38,10 +38,40 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model = model.ResNet18().to(device) #lance une nouvelle instance du modèle, ca réinitialise tout.
 print(model)
 
+#Suivi des paramètres avec un dictionnaire (pour pouvoir les utiliser en paramètre)
+
+#Syntaxe pour futur grid search
+
+config_counter = 1
+num_epochs = 100
+learning_rate = 0.1
+batch_size = 128
+num_blocks = 4
+base_channels = 64
+kernel_size = 3
+momentum = 0.9
+weight_decay = 5e-4
+step_size = 30
+gamma = 0.1
+
+config = {
+    "config_counter": config_counter,
+    "num_epochs": num_epochs,
+    "learning_rate": learning_rate,
+    "batch_size": batch_size,
+    "num_blocks": num_blocks,
+    "base_channels": base_channels,
+    "kernel_size": kernel_size,
+    "momentum": momentum,
+    "weight_decay": weight_decay,
+    "step_size": step_size,
+    "gamma": gamma
+}
+
 #Choix de la fonction d'activation, de l'optimizer et du scheduler
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(model.parameters(), lr=0.1, momentum=0.9, weight_decay=5e-4)
-scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=30, gamma=0.1)
+optimizer = optim.SGD(model.parameters(), lr=config[learning_rate], momentum=config[momentum], weight_decay=config[weight_decay])
+scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=config[step_size], gamma=config[gamma])
 
 
 #Num epochs par défaut
@@ -50,9 +80,11 @@ num_epochs = 20
 #Liste pour stocker les résultats (loss, acc)
 train_losses, train_acc_list, val_losses,  val_acc_list = [], [], [], []
 
+#entrainement du modèle
+
 for epoch in range(num_epochs):
     model.train()
-    running_loss = 0.0
+    train_running_loss = 0.0
     correct, total = 0, 0
     for inputs, labels in train_loader:
         inputs, labels = inputs.to(device), labels.to(device)
@@ -62,27 +94,33 @@ for epoch in range(num_epochs):
         loss.backward()
         optimizer.step()
         
-        running_loss += loss.item() * inputs.size(0)
+        train_running_loss += loss.item() * inputs.size(0)
         _, predicted = outputs.max(1)
         total += labels.size(0)
         correct += predicted.eq(labels).sum().item()
     
-    train_loss = running_loss / len(train_loader.dataset)
+    train_loss = train_running_loss / len(train_loader.dataset)
     train_acc = 100. * correct / total
     train_losses.append(train_loss)
     train_acc_list.append(train_acc)
 
+#évaluation des train et val
+
     model.eval()
     correct, total = 0, 0
+    val_running_loss = 0
     with torch.no_grad():
         for inputs, labels in val_loader:
             inputs, labels = inputs.to(device), labels.to(device)
             outputs = model(inputs)
+            loss = criterion(outputs, labels)
+
+            val_running_loss += loss.item() * inputs.size(0)
             _, predicted = outputs.max(1)
             total += labels.size(0)
             correct += predicted.eq(labels).sum().item()
 
-    val_loss = running_loss / len(val_loader.dataset)
+    val_loss = val_running_loss / len(val_loader.dataset)
     val_acc = 100. * correct / total
     val_losses.append(val_loss)
     val_acc_list.append(val_acc)
@@ -90,6 +128,14 @@ for epoch in range(num_epochs):
     scheduler.step()
     print(f'Epoch [{epoch+1}/{num_epochs}] Train Loss: {train_loss:.4f} | Val Loss: {val_loss:.4f} | Train Acc: {train_acc:.2f}% | Val Acc: {val_acc:.2f}%')
     
+
+
+
+
+config_str = f"config{config_counter}_epochs{num_epochs}_learningrate{learning_rate}_batchsize{batch_size}_numblocks{num_blocks}_basechannels{base_channels}_kernelsize{kernel_size}"
+print(f"Running config {config_str}")
+
+plt.suptitle(config_str, fontsize=8)
 
 plt.figure(figsize=(12,5))
 plt.subplot(1,2,1)
