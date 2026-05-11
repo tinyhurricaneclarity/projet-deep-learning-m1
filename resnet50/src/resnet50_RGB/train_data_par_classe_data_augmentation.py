841e078de41db9c11da6e5732a7669aca3ad1c28
Lea Nguyen <lea.nguyen.1@etu.u-bordeaux.fr>
2026-05-11 11:23:17 +0200

    resulats resnet18 MS

--- resnet50/src/resnet50_RGB/train_data_par_classe_data_augmentation.py
+++ resnet50/src/resnet50_RGB/train_data_par_classe_data_augmentation.py
@@ -280,7 +280,7 @@ for (num_epochs, learning_rate, optimizer_name, scheduler_name, step_size, gamma
 
 #Résumé des résultats dans un ficher CSV
 df = pd.DataFrame(results_summary)
-df.to_csv("src/resnet50_RGB/results/grid_search_results_par_classe.csv", index=False)
+df.to_csv("src/resnet50_RGB/results/grid_search_results_par_classe_RGB_50.csv", index=False)
 print("CSV résultats.")
 
 # Affichage meilleur modèle
